3430c9da5a4cd0f7b6100bde24275cf3c2cb9e22
Thor Haugness <thorhaugness@gmail.com>
2026-07-15 00:42:18 +0200

    Rett GitHub Pages-adresse

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // Endre base til '/<repo-navn>/' når du publiserer på GitHub Pages
 export default defineConfig({
-  base: './',
+  base: '/vaeretsomvar/',
   plugins: [react()],
 });
